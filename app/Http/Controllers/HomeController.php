7408b2587d429c4fc63380cfc2c9ab49878e1e0e
Mario <lombardomario01@gmail.com>
2022-01-28 16:42:16 +0100

    Routes and Controller updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     
     public function home() {
+
+        $comics = Comic::all();
         
-        return view('pages.home');
+        return view('pages.home', compact('comics'));
     }
+
+    public function show($id) {
+
+        $comic = Comic::findOrFail($id);
+
+        return view('pages.showComic', compact('comic'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@home')->name('home');
+Route::get('comic/show/{id}', 'HomeController@show')->name('comic');
